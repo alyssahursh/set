fb2b41beed091410963a8d679bde06646e2447ab
Alyssa Hursh <akhursh@gmail.com>
2016-12-23 22:28:13 -0800

    Refactored using JS set class

--- src/app/collections/in_play.js
+++ src/app/collections/in_play.js
@@ -1,79 +1,34 @@
 import Backbone from 'backbone';
 import Card from 'app/models/card';
 
-
-// this should maybe be a model instead???
 const InPlay = Backbone.Collection.extend({
   model: Card,
 
-  initialize: function() {
-  },
-
-  // THIS ENTIRE THING COULD BE DRIED UP TO ONLY BE ONE FUNCTION, BUT I WENT THE LONG ROUTE TO START
+  // Check if all colors match or are different
   checkColors: function(cards) {
-    // check if they all have the same color
-    var colorAllSame =
-      cards[0].get("color") === cards[1].get("color") &&
-      cards[1].get("color") === cards[2].get("color");
-
-    // check if they all have different colors
-    this.colorAllDifferent = function(cards) {
-      var sortedValues = [cards[0].get("color"), cards[1].get("color"), cards[2].get("color")].sort();
-      return sortedValues[0] === "green" && sortedValues[1] === "purple" && sortedValues[2] === "red";
-    };
-
-    return colorAllSame || this.colorAllDifferent(cards);
+    var colorSet = new Set([ cards[0].get("color"), cards[1].get("color"), cards[2].get("color") ]);
+    return colorSet.size === 1 || colorSet.size === 3;
   },
 
   // Check if all numbers match or are different
   checkShapes: function(cards) {
-    // check if they all have the same fill
-    var shapeAllSame =
-      cards[0].get("shape") === cards[1].get("shape") &&
-      cards[1].get("shape") === cards[2].get("shape");
-
-    // check if they all have different shapes
-    this.shapeAllDifferent = function(cards) {
-      var sortedValues = [cards[0].get("shape"), cards[1].get("shape"), cards[2].get("shape")].sort();
-      return sortedValues[0] === "diamond" && sortedValues[1] === "oval" && sortedValues[2] === "rectangle";
-    };
-
-    return shapeAllSame || this.shapeAllDifferent(cards);
+    var shapeSet = new Set([ cards[0].get("shape"), cards[1].get("shape"), cards[2].get("shape") ]);
+    return shapeSet.size === 1 || shapeSet.size === 3;
   },
 
   // Check if all numbers match or are different
   checkNumbers: function(cards) {
-    // check if they all have the same number
-    var numberAllSame =
-      cards[0].get("number") === cards[1].get("number") &&
-      cards[1].get("number") === cards[2].get("number");
-
-    // check if they all have different numbers
-    this.numberAllDifferent = function(cards) {
-      var sortedValues = [cards[0].get("number"), cards[1].get("number"), cards[2].get("number")].sort();
-      return sortedValues[0] === 1 && sortedValues[1] === 2 && sortedValues[2] === 3;
-    };
-
-    return numberAllSame || this.numberAllDifferent(cards);
+    var numberSet = new Set([ cards[0].get("number"), cards[1].get("number"), cards[2].get("number") ]);
+    return numberSet.size === 1 || numberSet.size === 3;
   },
 
   // Check if all fills match or are different
   checkFills: function(cards) {
-    // check if they all have the same fill
-    var fillAllSame =
-      cards[0].get("fill") === cards[1].get("fill") &&
-      cards[1].get("fill") === cards[2].get("fill");
-
-    // check if they all have different fills
-    this.fillAllDifferent = function(cards) {
-      var sortedValues = [cards[0].get("fill"), cards[1].get("fill"), cards[2].get("fill")].sort();
-      return sortedValues[0] === "empty" && sortedValues[1] === "solid" && sortedValues[2] === "stripe";
-    };
-
-    return fillAllSame || this.fillAllDifferent(cards);
+    var fillSet = new Set([ cards[0].get("fill"), cards[1].get("fill"), cards[2].get("fill") ]);
+    return fillSet.size === 1 || fillSet.size === 3;
   },
 
-  // We need a function to tell us if three clicked cards are a set
+  // Check if an array of three cards is a set
   isSet: function(cards) {
     return (this.checkColors(cards) && this.checkShapes(cards) && this.checkNumbers(cards) && this.checkFills(cards));
   },
@@ -86,7 +41,6 @@ const InPlay = Backbone.Collection.extend({
         for (var z = y + 1; z < this.length; z++) {
           if (this.isSet([this.at(x), this.at(y), this.at(z)])) {
             sets.push([x, y, z]);
-            console.log(this.at(x).attributes, this.at(y).attributes, this.at(z).attributes);
             setCount += 1;
           }
         }
