90f476d893432142c368ac1f5c70ee568cb36f14
Alyssa Hursh <akhursh@gmail.com>
2016-12-25 21:31:47 -0800

    fix

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,11 +7,16 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    loaders: [
+     { test: /\.js$/,
+       loader: 'babel-loader',
+       exclude: /node_modules/
+     },
+     { test: /\.css$/, loader: "style!css" },
+     { test: /\.(png|jpg|jpeg|gif|woff)$/, loader: 'url?limit=8192' },
+     { test: /\.(otf|eot|ttf)$/, loader: "file?prefix=font/" },
+     { test: /\.svg$/, loader: "file" }
+   ],
   },
   devtool: 'source-map',
   devServer: {
